095704270fc9edaf5b1373e607e9df8ffc7d8bc3
nik <volsknik@gmail.com>
2020-10-20 15:31:47 +0300

    checkout -b

---
+++ src/test/java/ui/smoke/login/UserCabinetDropdownTest.java
@@ -0,0 +1,20 @@
+package ui.smoke.login;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ui.OnixAssert;
+import ui.engine.OnixLocator;
+import ui.user_mode.page_objects.UserCabinetDropdown;
+
+public class UserCabinetDropdownTest extends JsaLoginBaseTestRunner {
+    @Test(dataProvider = "getUserCabinetDropdownLocators")
+    public void userCabinetDropdownTest(OnixLocator locator) {
+        homeLoginModePO.openUserDropDown();
+        new OnixAssert(driver).checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUserCabinetDropdownLocators() {
+        return UserCabinetDropdown.Locator.values();
+    }
+}
