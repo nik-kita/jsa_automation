60eefc8d7bd7027c7dcb6ba594ad627ef29a44fd
nik <volsknik@gmail.com>
2020-12-03 21:02:31 +0200

    onixUiAssert.assertAll() in every @Test

--- src/test/java/test_package/ui/smoke/LogoutModeMainUiTest.java
+++ src/test/java/test_package/ui/smoke/LogoutModeMainUiTest.java
@@ -9,56 +9,67 @@ public class LogoutModeMainUiTest extends OnixUiTestRunner {
     @Test
     public void mainPageTest() {
         openSite(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickGetStarted() {
         openSite().clickGetStartedButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickPlayStore() {
         openSite().clickPlayStoreBannerButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickAppStore() {
         openSite().clickAppStoreBannerButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickMyInstagram() {
         openSite().clickMyInstagramBannerButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickMyFacebook() {
         openSite().clickMyFacebookBannerButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickPlayStoreInDownloadSection() {
         openSite().clickPlayStoreButtonInDownloadSection(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickAppStoreButtonInDownloadSection() {
         openSite().clickAppStoreButtonInDownloadSection(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickAboutChallenges() {
         openSite().clickAboutChallengesButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickSeeMoreTransformations() {
         openSite().clickSeeMoreTransformationsButton(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void signUpForFamousDailyEmails() {
         openSite().cheaterClickFamousDailyEmail(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
@@ -69,56 +80,75 @@ public class LogoutModeMainUiTest extends OnixUiTestRunner {
     @Test
     public void clickTermsOfService() {
         openSite().clickFooterTermsOfService(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickCookiesPolicy() {
         openSite().clickFooterCookiesPolicy(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickPrivacyPolicy() {
         openSite().clickFooterPrivatePolicy(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickFooterBlog() {
         openSite().clickFooterBlog(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickBuReports() {
         openSite().clickFooterBugReports(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickContactUs() {
         openSite().clickFooterContactUs(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickAccess() {
         openSite().clickFooterAccess(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickFaqs() {
         openSite().clickFooterFAQs(onixUiAssert);
+        onixUiAssert.assertAll();
     }
 
     @Test
     public void clickFooterFacebook() {
         openSite().clickFooterFacebook(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickFooterInstagram() {
         openSite().clickFooterInstagram(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickFooterSpotify() {
         openSite().clickFooterSpotify(onixUiAssert);
+        onixUiAssert.assertAll();
     }
+
     @Test
     public void clickFooterPlayStore() {
         openSite().clickFooterPlayStore(onixUiAssert);
         onixUiAssert.assertAll();
     }
+
     @Test
     public void clickFooterAppStore() {
         openSite().clickFooterAppStore(onixUiAssert);
